e32cf30b0dab9134051b57c1f624eadfecfae17d
muhammad.yaseen <github@imtiaz.com.pk>
2026-01-09 14:30:00 +0500

    Document Loaders, Text Splitters, Embeddings, Vector Stores and Retrieval using the Retrieval Chain.

--- retrieval-chain.py
+++ retrieval-chain.py
@@ -1,36 +1,35 @@
 from dotenv import load_dotenv
 load_dotenv()
-from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.documents import Document
-from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter 
-from langchain_openai import OpenAIEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores.faiss import FAISS
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_classic.chains.retrieval import create_retrieval_chain
 
 def get_documents_from_web(url):
     loader = WebBaseLoader(url)
     docs = loader.load()
     splitter = RecursiveCharacterTextSplitter(
-        chunk_size=200,
+        chunk_size=150,
         chunk_overlap=20
     )
-    splitted_doc = splitter.split_documents(docs)
+    splitted_doc = splitter.create_documents(docs)
     return splitted_doc
 
-
 def create_db(docs):
-    embedding = OpenAIEmbeddings()
+    embedding = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
     vectorStore = FAISS.from_documents(documents=docs,embedding=embedding)
     return vectorStore
 
 def create_chain(vectorStore):
-    model = ChatOpenAI(
-        model="gpt-5-nano",
-        temperature=0.7,
-        verbose=True
+    model = ChatOllama(
+        model="llama3.2:3b", 
+        temperature=0.2,
     )
 
     prompt = ChatPromptTemplate.from_template(
@@ -39,30 +38,22 @@ def create_chain(vectorStore):
             Context : {context}
             Question: {input}    
     """)
-
-    # chain = prompt | model
-
-    chain = create_stuff_documents_chain(
-        llm = model,
-        prompt=prompt
-    )
     
-    #search_kwargs={"k": 3}
+    chain = create_stuff_documents_chain(llm=model,prompt=prompt)
+    
     retriever = vectorStore.as_retriever()
-    chainRetriever = create_retrieval_chain(retriever,combine_docs_chain=chain)
+    
+    chainRetriever = create_retrieval_chain(retriever=retriever,combine_docs_chain=chain)
     
     return chainRetriever
-    
-    
 
 
 docs = get_documents_from_web("https://developer.android.com/guide/components/activities/activity-lifecycle")
 vectorStore = create_db(docs)
 chain = create_chain(vectorStore)
 
-
-response = chain.invoke(
-    {"input":"Explain the Android Activity Lifecycle"}
-    )
+response = chain.invoke({
+    "input":"what is onResume in lifecycle"
+})    
 
 print(response["answer"])
